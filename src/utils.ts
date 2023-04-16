a1fde03ec6998ceea2b62db912b313909072e667
he <he@everything.com>
2023-04-16 17:00:14 -0500

    feat: allow scripts in views

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,21 @@ export function renderContent(
 	content: string,
 	selector = window.PineconeRouter.settings.viewSelector ?? '#app'
 ) {
+	// replace with xpath?
+	var domParser = new DOMParser()
+	var view = domParser.parseFromString(content, 'text/html')
+	var scriptsClassname = 'app-scripts'
+	document.querySelectorAll('.' + scriptsClassname).forEach(script => script.remove())
+	var scripts = view.querySelectorAll('script')
+	scripts.forEach(script => {
+		script.remove()
+		var scriptToInsert = document.createElement('script')
+		scriptToInsert.className = 'app-scripts'
+		scriptToInsert.textContent = script.textContent
+		document.body.appendChild(scriptToInsert)
+	})
 	// replace the content of the selector with the fetched content
-	document.querySelector(selector)!.innerHTML = content
+	document.querySelector(selector)!.replaceChildren(...Array.prototype.slice.call(view.body.childNodes, 0))
 	// @ts-ignore
 	document.querySelector('[autofocus]')?.focus()
 }
