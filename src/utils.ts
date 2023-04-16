a84af0e29402da62eefe14a13978f1a4c9b21263
he <he@everything.com>
2023-04-16 18:50:58 -0500

    feat: render scripts inside views

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+var scriptsClassname = 'app-scripts'
+
 /**
  * This will replace the content fetched from `path` into `selector`.
  * The content is assumed to not be an entire html page but a chunk of it.
@@ -6,8 +8,23 @@ export function renderContent(
 	content: string,
 	selector = window.PineconeRouter.settings.viewSelector ?? '#app'
 ) {
-	// replace the content of the selector with the fetched content
-	document.querySelector(selector)!.innerHTML = content
+	// create view placeholder from fetched content
+	var view = document.createElement('div')
+	view.innerHTML = content;
+
+	// remove all previous view scripts
+	document.querySelectorAll('.' + scriptsClassname).forEach(script => script.remove())
+	// insert all new view scripts 
+	view.querySelectorAll('script').forEach(script => {
+		script.remove()
+		var scriptClone = document.createElement('script')
+		scriptClone.className = scriptsClassname
+		scriptClone.textContent = script.textContent
+		document.body.appendChild(scriptClone)
+	})
+
+	// replace previous view content with the fetched content
+	document.querySelector(selector)!.replaceChildren(...Array.from(view.childNodes))
 	// @ts-ignore
 	document.querySelector('[autofocus]')?.focus()
 }
